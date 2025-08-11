7657e922f7cb47bdb360c7ade6f2048a2a5b646f
Yerim Moon <myr9901@gmail.com>
2025-08-10 21:12:03 -0700

    ui: remove navbar outline focus

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -23,82 +23,6 @@ import {
 } from "@heroicons/react/24/outline";
 
 import { navigation } from "@/components/layout/navigation";
-// const navigation = {
-//   categories: [
-//     {
-//       id: "coffee",
-//       name: "Coffee",
-//       featured: [
-//         {
-//           name: "New Arrivals",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindcss.com/plus-assets/img/ecommerce-images/mega-menu-category-01.jpg",
-//           imageAlt:
-//             "Models sitting back to back, wearing Basic Tee in black and bone.",
-//         },
-//         {
-//           name: "Shop All Coffee",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindcss.com/plus-assets/img/ecommerce-images/mega-menu-category-02.jpg",
-//           imageAlt:
-//             "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
-//         },
-//       ],
-//       sections: [
-//         {
-//           id: "shop-all-coffee",
-//           name: "Shop All Coffee",
-//           href: "#",
-//         },
-//         {
-//           id: "roast-profile",
-//           name: "Roast Profile",
-//           items: [
-//             { name: "Light", href: "#" },
-//             { name: "Medium", href: "#" },
-//             { name: "Dark", href: "#" },
-//             { name: "Decaf", href: "#" },
-//           ],
-//         },
-//       ],
-//     },
-//     {
-//       id: "matcha",
-//       name: "Matcha",
-//       featured: [
-//         {
-//           name: "New Arrivals",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
-//           imageAlt:
-//             "Drawstring top with elastic loop closure and textured interior padding.",
-//         },
-//         {
-//           name: "Shop All Matcha",
-//           href: "#",
-//           imageSrc:
-//             "https://tailwindcss.com/plus-assets/img/ecommerce-images/category-page-02-image-card-06.jpg",
-//           imageAlt:
-//             "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
-//         },
-//       ],
-//       sections: [
-//         {
-//           id: "shop-all-matcha",
-//           name: "Shop All Matcha",
-//           href: "#",
-//         },
-//       ],
-//     },
-//   ],
-//   pages: [
-//     { name: "Sale", href: "#" },
-//     { name: "Stores", href: "#" },
-//   ],
-// };
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
@@ -285,7 +209,11 @@ export default function Navbar() {
                   {navigation.categories.map((category) => (
                     <Popover key={category.name} className="flex">
                       <div className="relative flex">
-                        <PopoverButton className="group relative flex items-center justify-center text-sm font-medium text-gray-700 transition-colors duration-200 ease-out hover:text-gray-800 data-open:text-[var(--brand-color-dark)]">
+                        <PopoverButton
+                          className="group relative flex items-center justify-center text-sm font-medium 
+                        text-gray-700 transition-colors duration-200 ease-out 
+                        hover:text-gray-800 data-open:text-[var(--brand-color-dark)] focus:outline-none"
+                        >
                           {category.name}
                           <span
                             aria-hidden="true"
@@ -382,7 +310,11 @@ export default function Navbar() {
                   {navigation.pages.map((page) => (
                     <Popover key={page.name} className="flex">
                       <div className="relative flex">
-                        <PopoverButton className="group relative flex items-center justify-center text-sm font-medium text-gray-700 transition-colors duration-200 ease-out hover:text-gray-800 data-open:text-[var(--brand-color-dark)]">
+                        <PopoverButton
+                          className="group relative flex items-center justify-center text-sm font-medium 
+                        text-gray-700 transition-colors duration-200 ease-out hover:text-gray-800 
+                        data-open:text-[var(--brand-color-dark)] focus:outline-none"
+                        >
                           {page.name}
                           <a
                             key={page.name}
